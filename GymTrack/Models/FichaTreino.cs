2634d12634493d18a6d10c0588346eb1e427fb66
yagolizacchi <yagolizacchi@gmail.com>
2026-04-26 22:37:36 -0300

    Make Aluno and PlanoTreino properties nullable

--- GymTrack/Models/FichaTreino.cs
+++ GymTrack/Models/FichaTreino.cs
@@ -1,15 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GymTrack.Models;
 
 public class FichaTreino
 {
     public int Id { get; set; }
+
     public DateTime DataCriacao { get; set; } = DateTime.Now;
-    
+
+    [Required(ErrorMessage = "A ficha deve estar vinculada a um aluno")]
     public int AlunoId { get; set; }
-    public Aluno Aluno { get; set; } = null!;
-    
+    public Aluno? Aluno { get; set; }
+
+    [Required(ErrorMessage = "A ficha deve possuir um plano de treino")]
     public int PlanoTreinoId { get; set; }
-    public PlanoTreino PlanoTreino { get; set; } = null!;
+    public PlanoTreino? PlanoTreino { get; set; }
 
     public ICollection<Exercicio> Exercicios { get; set; } = new List<Exercicio>();
 }
