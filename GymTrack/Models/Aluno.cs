8424692c311bcdf2aeec545385f145856562755e
yagolizacchi <yagolizacchi@gmail.com>
2026-04-26 22:33:53 -0300

    Add validation attributes to Aluno properties

--- GymTrack/Models/Aluno.cs
+++ GymTrack/Models/Aluno.cs
@@ -1,13 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GymTrack.Models;
 
 public class Aluno
 {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
     public string Nome { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
     public string Email { get; set; } = string.Empty;
+
     public DateTime DataNascimento { get; set; }
+    
     public DateTime DataMatricula { get; set; } = DateTime.Now;
 
     public ICollection<FichaTreino> FichasTreino { get; set; } = new List<FichaTreino>();
-
 }
