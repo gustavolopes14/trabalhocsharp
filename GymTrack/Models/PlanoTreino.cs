ecc313bb79e7de42efa7719f56e9cfb5ed1499e0
yagolizacchi <yagolizacchi@gmail.com>
2026-04-26 22:39:19 -0300

    Add validation to Nome property in PlanoTreino
    Added validation attributes for Nome property.

--- GymTrack/Models/PlanoTreino.cs
+++ GymTrack/Models/PlanoTreino.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GymTrack.Models;
 
 public class PlanoTreino
 {
     public int Id { get; set; }
-    public string Nome {get; set;} = string.Empty;
+
+    [Required(ErrorMessage = "O nome do plano é obrigatório")]
+    [StringLength(100)]
+    public string Nome { get; set; } = string.Empty;
+
+    [StringLength(500)]
     public string Descricao { get; set; } = string.Empty;
 
     public ICollection<FichaTreino> FichasTreino { get; set; } = new List<FichaTreino>();
