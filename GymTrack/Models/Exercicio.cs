65d11125eb2997de4c1b675e7d270ca00634a182
yagolizacchi <yagolizacchi@gmail.com>
2026-04-26 22:35:47 -0300

    Add data annotations to Exercicio model properties

--- GymTrack/Models/Exercicio.cs
+++ GymTrack/Models/Exercicio.cs
@@ -1,11 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
 namespace GymTrack.Models;
 
 public class Exercicio
 {
     public int Id { get; set; }
+
+    [Required]
     public string Nome { get; set; } = string.Empty;
+
     public string Descricao { get; set; } = string.Empty;
+
+    [Required]
     public string GrupoMuscular { get; set; } = string.Empty;
 
+    [JsonIgnore]
     public ICollection<FichaTreino> FichasTreino { get; set; } = new List<FichaTreino>();
 }
