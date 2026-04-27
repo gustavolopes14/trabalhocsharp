1f16b8a5b6d9099af73fbcbccd3e89a1c04a73ac
yagolizacchi <yagolizacchi@gmail.com>
2026-04-26 22:27:47 -0300

    Add FichaTreino and Aluno configurations to DbContext

--- GymTrack/Data/AppDbContext.cs
+++ GymTrack/Data/AppDbContext.cs
@@ -11,4 +11,17 @@ public class AppDbContext : DbContext
     public DbSet<PlanoTreino> PlanosTreino { get; set; }
     public DbSet<Exercicio> Exercicios { get; set; }
     public DbSet<FichaTreino> FichasTreino { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<FichaTreino>()
+            .HasMany(f => f.Exercicios)
+            .WithMany(e => e.FichasTreino)
+            .UsingEntity(j => j.ToTable("FichaExercicios"));
+
+        modelBuilder.Entity<Aluno>()
+            .HasIndex(a => a.Email)
+            .IsUnique();
+    }
 }
